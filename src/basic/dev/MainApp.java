06cffeae30ca772a10f8546aae57fa856a7146e3
phongz <phongnguyenzzz991@gmai.com>
2021-10-27 14:52:01 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,56 @@
+package basic.dev;
+/**
+ * viết chương trình nhập 2 số nguyên a,b:
+ * @author ADmin
+ *
+ */
+
+public class MainApp {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a = 15, b = 10;
+	/**
+	 * tinh tong
+	 */
+	int tong = a+b;
+	System.out.println("tong:"+tong);
+	/**
+	 * tinh hieu
+	 */
+	int hieu = a-b;
+	System.out.println("hieu:"+hieu);
+	/**
+	 * tinh tich
+	 */
+	int tich = a*b;
+	System.out.println("tich:"+tich);
+	/**
+	 * tinh thuong
+	 */
+	int thuong = a/b;
+	System.out.println("thuong:"+thuong);
+	/**
+	 * gan C
+	 */
+	boolean c = a > b;
+	System.out.println("a lon hon b: " + c);
+	c = a < b;
+	System.out.println("a nho hon b : " + c);
+	c = a <= b;
+	System.out.println("a nho hon hoac bang b : " + c);
+	c = a >= b;
+	System.out.println("a lon hon hoac bang b : " +c);
+	
+	
+	
+
+	
+
+	
+	
+	
+	
+	
+	}
+}
+
